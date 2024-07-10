b55e08ce9e8aea91026aef60530a706e1a4e9b21
Ewrdad <ewrdad@protonmail.com>
2024-07-10 10:11:58 +0100

    added template

---
+++ template/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Simple Calculator</h1>
+      </div>
+      <div className="card">
+        {/**
+         Write code here
+         */}
+        <p>Calculator here</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
